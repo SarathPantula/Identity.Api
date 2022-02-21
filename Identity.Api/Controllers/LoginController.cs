908f242634e6512ab25282843a28664ac875b316
Sarath Pantula <sarath.pantula@gmail.com>
2022-02-21 08:06:26 +0530

    Login

---
+++ Identity.Api/Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using Identity.Api.IdentityManagers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly IIdentityManager _identityManager;
+        public LoginController(Func<string, IIdentityManager> identityManager)
+        {
+            _identityManager = identityManager("Validation");
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("")]
+        public IActionResult Login()
+        {
+            return Ok(_identityManager.CanLogin());
+        }
+    }
+}
